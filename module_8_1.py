bcc249747fb5f2ff26e6b8eadc6cc0b5ebcaa895
venya668 <veniaminvolkov802@gmail.com>
2024-11-06 21:51:23 +1100

    Add files via upload

---
+++ module_8_1.py
@@ -0,0 +1,13 @@
+def add_everything_up(a, b):
+    try:
+        if isinstance(a, (int, float)) and isinstance(b, (int, float)):
+            return a + b
+        elif isinstance(a, str) and isinstance(b, str):
+            return a + b
+        else:
+            raise TypeError
+    except TypeError:
+        return str(a)+str(b)
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
